6cf6d0fce6c257da238eefbc8357eebd4c6c32cc
dujiajun <cqdujiajun@outlook.com>
2020-12-11 10:10:15 +0000

    Allow hiding my topic to ignored

--- assets/javascripts/initializers/enhanced-ignore.js.es6
+++ assets/javascripts/initializers/enhanced-ignore.js.es6
@@ -1,12 +1,7 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { gte, or } from "@ember/object/computed";
 
 function initializeEnhancedIgnore(api) {
   // https://github.com/discourse/discourse/blob/master/app/assets/javascripts/discourse/lib/plugin-api.js.es6
-  api.modifyClass("controller:preferences/users",{
-    userIsMemberOrAbove: gte("model.trust_level", 1),
-    ignoredEnabled: or("userIsMemberOrAbove", "model.staff"),
-  })
 
   api.modifyClass("controller:preferences/notifications",{
     actions: {
--- plugin.rb
+++ plugin.rb
@@ -2,11 +2,12 @@
 
 # name: enhanced-ignore
 # about: enhanced plugin
-# version: 0.1
+# version: 0.2
 # authors: dujiajun
 # url: https://github.com/dujiajun/enhanced-ignore
 
-DiscoursePluginRegistry.serialized_current_user_fields << 'enhanced_ignore'
+DiscoursePluginRegistry.serialized_current_user_fields << 'hide_ignored_topics'
+DiscoursePluginRegistry.serialized_current_user_fields << 'hide_my_topics_to_ignored'
 
 register_asset 'stylesheets/common/enhanced-ignore.scss'
 register_asset 'javascripts/discourse/templates/connectors/user-preferences-notifications/enhanced-ignore-preferences.hbs'
@@ -21,27 +22,44 @@ load File.expand_path('lib/enhanced-ignore/engine.rb', __dir__)
 
 after_initialize do
   # https://github.com/discourse/discourse/blob/master/lib/plugin/instance.rb
-  User.register_custom_field_type 'enhanced_ignore', :boolean
-  register_editable_user_custom_field :enhanced_ignore
+  User.register_custom_field_type 'hide_ignored_topics', :boolean
+  User.register_custom_field_type 'hide_my_topics_to_ignored', :boolean
+  register_editable_user_custom_field :hide_ignored_topics
+  register_editable_user_custom_field :hide_my_topics_to_ignored
 
   if SiteSetting.enhanced_ignore_enabled?
-
+    old = UserCustomField.where(name: 'enhanced_ignore')
+    if old
+      old.update_all(name: 'hide_ignored_topics')
+    end
     class ::TopicQuery
       module OverridingDefaultResults
 
         def default_results(options = {})
-
-          if @user and @user.custom_fields['enhanced_ignore']
-            results = super(options).where("topics.user_id NOT IN ( 
+            results = super(options)
+          if @user and @user.custom_fields['hide_ignored_topics']
+            results = results.where("topics.user_id NOT IN ( 
                                           SELECT ignored_user_id 
                                           FROM ignored_users 
                                           WHERE ignored_users.user_id = ?
                                           )", @user.id)
-            results
-          else
-            super(options)
           end
-
+          if @user
+            results = results.where("? NOT IN 
+                                    ( 
+                                    SELECT ignored_user_id 
+                                    FROM ignored_users
+                                    WHERE ignored_users.user_id = topics.user_id
+                                    AND EXISTS (
+                                      SELECT user_id 
+                                      FROM user_custom_fields 
+                                      WHERE user_custom_fields.name = 'hide_my_topics_to_ignored' 
+                                      AND user_custom_fields.value = 'true'
+                                      AND user_custom_fields.user_id = topics.user_id
+                                    )
+                                    )", @user.id)
+          end
+          results
         end
       end
       prepend OverridingDefaultResults
@@ -50,9 +68,8 @@ after_initialize do
     class ::Guardian
       module OverridingCanIgnoreUser
 
-        def can_ignore_users?
-          return false if anonymous?
-          @user.staff? || @user.trust_level >= TrustLevel.levels[:basic]
+        def can_ignore_user?(target_user)
+          can_ignore_users? && @user.id != target_user.id && !target_user.admin?
         end
 
       end

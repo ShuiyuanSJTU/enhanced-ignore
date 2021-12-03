7d8e0f6a8bd8a16e7c7bdb753ff5f651dcd5694f
Du Jiajun <jiajun_du@outlook.com>
2021-12-03 08:14:16 +0000

    style

--- plugin.rb
+++ plugin.rb
@@ -16,21 +16,20 @@ PLUGIN_NAME ||= 'EnhancedIgnore'
 
 after_initialize do
 
-   User.register_custom_field_type 'hide_ignored_topics', :boolean
+  User.register_custom_field_type 'hide_ignored_topics', :boolean
    register_editable_user_custom_field :hide_ignored_topics
 
    # TODO: 回复被屏蔽用户的主题不在时间线上提醒 PostAlerter, AboutController
    # TODO: 不显示回复被屏蔽用户的帖子 TopicView
    TopicQuery.add_custom_filter(:ignore_users) do |results, topic_query|
-      if SiteSetting.enhanced_ignore_enabled?
-         user = topic_query.user
-         if user and user.custom_fields['hide_ignored_topics']
-            ignored_user_ids = IgnoredUser.where(user: user).select(:ignored_user_id)
-            results = results.where.not(user_id: ignored_user_ids)
-         end
-      end
+     if SiteSetting.enhanced_ignore_enabled?
+       user = topic_query.user
+        if user and user.custom_fields['hide_ignored_topics']
+          ignored_user_ids = IgnoredUser.where(user: user).select(:ignored_user_id)
+           results = results.where.not(user_id: ignored_user_ids)
+        end
+     end
       results
    end
-   
 
 end

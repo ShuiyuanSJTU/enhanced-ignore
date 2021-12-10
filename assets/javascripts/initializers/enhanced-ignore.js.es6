f5de0f5f6e305bca08e2dad41a24d4e45495a9df
Du Jiajun <jiajun_du@outlook.com>
2021-12-10 07:13:12 +0000

    ignored user cannot reply to me

--- assets/javascripts/initializers/enhanced-ignore.js.es6
+++ assets/javascripts/initializers/enhanced-ignore.js.es6
@@ -2,10 +2,9 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 
 function initializeEnhancedIgnore(api) {
   // https://github.com/discourse/discourse/blob/master/app/assets/javascripts/discourse/lib/plugin-api.js.es6
-
-  api.modifyClass("controller:preferences/notifications",{
+  api.modifyClass("controller:preferences/notifications", {
     actions: {
-      save () {
+      save() {
         this.get('saveAttrNames').push('custom_fields')
         this._super()
       }
--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,6 @@
 # authors: Jiajun Du
 # url: https://github.com/ShuiyuanSJTU/enhanced-ignore
 
-DiscoursePluginRegistry.serialized_current_user_fields << 'hide_ignored_topics'
-
 register_asset 'javascripts/discourse/templates/connectors/user-preferences-notifications/enhanced-ignore-preferences.hbs'
 
 enabled_site_setting :enhanced_ignore_enabled
@@ -16,11 +14,16 @@ PLUGIN_NAME ||= 'EnhancedIgnore'
 
 after_initialize do
 
+  DiscoursePluginRegistry.serialized_current_user_fields << 'hide_ignored_topics'
+  DiscoursePluginRegistry.serialized_current_user_fields << 'ignored_user_cannot_reply_to_me'
   User.register_custom_field_type 'hide_ignored_topics', :boolean
-   register_editable_user_custom_field :hide_ignored_topics
+  User.register_custom_field_type 'ignored_user_cannot_reply_to_me', :boolean
+  register_editable_user_custom_field :hide_ignored_topics
+  register_editable_user_custom_field :ignored_user_cannot_reply_to_me
 
    # TODO: 回复被屏蔽用户的主题不在时间线上提醒 PostAlerter, AboutController
-   # TODO: 不显示回复被屏蔽用户的帖子 TopicView
+
+   # 不在主题列表显示被屏蔽用户的帖子
    TopicQuery.add_custom_filter(:ignore_users) do |results, topic_query|
      if SiteSetting.enhanced_ignore_enabled?
        user = topic_query.user
@@ -32,4 +35,19 @@ after_initialize do
       results
    end
 
+   # 不让被屏蔽用户回复我
+   module OverrideTopicGuardian
+     def can_create_post_on_topic?(topic)
+       return false if !super
+       if SiteSetting.enhanced_ignore_enabled? && topic.user && topic.user.custom_fields['ignored_user_cannot_reply_to_me']
+         ignored_user_ids = IgnoredUser.where(user: topic.user).select(:ignored_user_id)
+         return !ignored_user_ids.exists?(ignored_user_id: @user)
+       end
+       true
+     end
+   end
+
+   class ::Guardian
+     prepend OverrideTopicGuardian
+   end
 end
